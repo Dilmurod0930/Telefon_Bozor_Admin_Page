4ad72118255ea22852bfb23fa10879c06a70f0fc
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-03 11:59:39 +0500

    Appga  Models.py  faylga  model  qo'shildi.

--- telefon/models.py
+++ telefon/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Telefon(models.Model):
+    name = models.CharField("Telefon nomi", max_length=100)
+    model = models.CharField("Model", max_length=100)
+    color = models.CharField("Rangi", max_length=50)
+    storage = models.IntegerField("Xotira (GB)")
+    ram = models.IntegerField("RAM (GB)")
+    os = models.CharField("Operatsion tizim", max_length=50)
+    image = models.ImageField("Rasm", upload_to='telefonlar/', null=True, blank=True)
+    description = models.TextField("Tavsif", blank=True, null=True)
+    created_at = models.DateTimeField("Qo‘shilgan vaqt", auto_now_add=True)
+    updated_at = models.DateTimeField("Yangilangan vaqt", auto_now=True)
+
+    
