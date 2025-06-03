6b08452f469e26912371d5b00ce595e32abd6339
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-03 12:06:39 +0500

    views  fayldagi  funksiayalr  to'g'irlandi

--- telefon/views.py
+++ telefon/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+from Online_Telefon_Bozor_P.telefon.models import Telefon
+
+
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
 
 def telefon_lst(request):
-    return render(request, 'telefon_lst.html')
+    tele = Telefon.objects.all()
+    context = {'tele': tele}
+    return render(request, 'telefon_lst.html', context =context)
 
-def telefon_info(request):
-    return render(request, 'telefon_info.html')
+def telefon_info(request, id):
+    tele = Telefon.objects.get(id=id)
+    context = {'tele': tele}
+    return render(request, 'telefon_info.html', context= context)
