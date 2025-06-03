a90450909bcd3f8f53880f9225a53abf40ab056d
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-03 12:03:11 +0500

    Models  faylga  o'zgartirish kiritildi

--- telefon/models.py
+++ telefon/models.py
@@ -5,6 +5,8 @@ class Telefon(models.Model):
     name = models.CharField("Telefon nomi", max_length=100)
     model = models.CharField("Model", max_length=100)
     color = models.CharField("Rangi", max_length=50)
+    discount_price = models.DecimalField("Chegirma narxi (so‘m)", max_digits=12, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField("Narxi (so‘m)", max_digits=12, decimal_places=2)
     storage = models.IntegerField("Xotira (GB)")
     ram = models.IntegerField("RAM (GB)")
     os = models.CharField("Operatsion tizim", max_length=50)
@@ -13,4 +15,9 @@ class Telefon(models.Model):
     created_at = models.DateTimeField("Qo‘shilgan vaqt", auto_now_add=True)
     updated_at = models.DateTimeField("Yangilangan vaqt", auto_now=True)
 
-    
+    class Meta:
+        verbose_name = "Telefon"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.name} {self.model} ({self.price})"
