99360655f981b414db9bda82ca09b954dbc24fc2
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-03 12:09:13 +0500

    URLS.py  fayl  to'g'irlandi

--- telefon/urls.py
+++ telefon/urls.py
@@ -1,5 +1,8 @@
 from  django.urls import path
-from .views import *
+from .views import main,  telefon_lst,  telefon_info
+
 urlpatterns = [
-    
+    path('', main, name='main'),
+    path('telefon_lst/', telefon_lst, name='telefon_lst'),
+    path('telefon/<int:id>', telefon_info, name='telefon_info'),
 ]
