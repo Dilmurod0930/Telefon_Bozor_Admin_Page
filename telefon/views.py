b0539dd61ceded0e77c56c0cbd0bf410931784e1
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-03 12:05:04 +0500

    Views  fayl  ichiga  funksiyalar  yaratildi. To'liq  kod  endi  yoziladi

--- telefon/views.py
+++ telefon/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main(request):
+    return render(request, 'main.html')
+
+def telefon_lst(request):
+    return render(request, 'telefon_lst.html')
+
+def telefon_info(request):
+    return render(request, 'telefon_info.html')
