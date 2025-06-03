210f60933d001096e84c8bebe6075b45fd481db5
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-03 15:27:36 +0500

    create fayl  yaratildi databasega  malumot  qo'shildi.

--- telefon/urls.py
+++ telefon/urls.py
@@ -1,8 +1,9 @@
 from  django.urls import path
-from .views import main,  telefon_lst,  telefon_info
+from .views import main,  telefon_lst,  telefon_info, telefon_create
 
 urlpatterns = [
     path('', main, name='main'),
-    path('telefon_lst', telefon_lst, name='telefon_lst'),
+    path('telefon_lst/', telefon_lst, name='telefon_lst'),
     path('telefon/<int:id>', telefon_info, name='telefon_info'),
+    path('telefon_create/', telefon_create, name='telefon_create'),
 ]
