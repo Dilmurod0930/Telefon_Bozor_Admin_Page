45cd555fd69dc3bba9771817f0346fd64166a112
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-03 21:33:21 +0500

    Update  page  qo'shildi

--- telefon/urls.py
+++ telefon/urls.py
@@ -1,9 +1,10 @@
 from  django.urls import path
-from .views import main,  telefon_lst,  telefon_info, telefon_create
+from .views import main,  telefon_lst,  telefon_info, telefon_create,  telefon_update
 
 urlpatterns = [
     path('', main, name='main'),
     path('telefon_lst/', telefon_lst, name='telefon_lst'),
     path('telefon/<int:id>', telefon_info, name='telefon_info'),
     path('telefon_create/', telefon_create, name='telefon_create'),
+    path('telefon_update/<int:id>/', telefon_update, name='telefon_update'),
 ]
