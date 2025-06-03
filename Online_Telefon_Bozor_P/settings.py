90723bbe27568b5c26a4a8e9b4c4b4d1d5f3d44c
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-03 12:12:11 +0500

    settings.py  postgrest   qo'shildi  database  yaratildi.

--- Online_Telefon_Bozor_P/settings.py
+++ Online_Telefon_Bozor_P/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Online_Telefon_Bozor_P.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online_d',
+        'USER': 'postgrest',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
