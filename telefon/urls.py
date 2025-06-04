89b6e99ed5f4da38ef7b31d93bdc97fbdfb5bb4c
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-04 06:48:11 +0500

    Urls.py  faylga  delete path qo'shildi

--- telefon/urls.py
+++ telefon/urls.py
@@ -1,5 +1,6 @@
 from  django.urls import path
-from .views import main,  telefon_lst,  telefon_info, telefon_create,  telefon_update
+from .views import main,  telefon_lst,  telefon_info, \
+    telefon_create,  telefon_update, telefon_delete
 
 urlpatterns = [
     path('', main, name='main'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('telefon/<int:id>', telefon_info, name='telefon_info'),
     path('telefon_create/', telefon_create, name='telefon_create'),
     path('telefon_update/<int:id>/', telefon_update, name='telefon_update'),
+    path('telefon_delete/<int:id>/', telefon_delete, name='telefon_delete'),
 ]
